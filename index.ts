412fc2265eb79add86a4d0a83d1fbfeaf2bb2526
James Quigley <jmquigley@outlook.com>
2017-02-21 08:53:05 -0600

    Fixes the index for use with typescript

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-const Deque = require('./lib/deque').Deque;
-const Queue = require('./lib/queue').Queue;
-const Stack = require('./lib/stack').Stack;
+import {Deque} from './lib/deque';
+import {Queue} from './lib/queue';
+import {Stack} from './lib/stack';
 
 export { Deque, Queue, Stack };
